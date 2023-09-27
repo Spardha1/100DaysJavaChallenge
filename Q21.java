b8d41db5371fd9bbd3a9d8de0b8cd04ba42ce70c
Spardha1 <80118289+Spardha1@users.noreply.github.com>
2023-09-27 23:26:13 +0530

    Add files via upload

---
+++ Q21.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Q21 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if(a==b){
+            System.out.println("yes it is a square");
+        }
+        else{
+            System.out.println("not a square");
+        }
+    }
+}
