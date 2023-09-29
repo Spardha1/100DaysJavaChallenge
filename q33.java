101e09c4a05bac52ded470df5aec51d0bad85752
Spardha1 <80118289+Spardha1@users.noreply.github.com>
2023-09-29 23:45:58 +0530

    Add files via upload

---
+++ q33.java
@@ -0,0 +1,10 @@
+public class q33 {
+   
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            System.out.println("Hello Java");
+        }
+    }
+}
+
+
