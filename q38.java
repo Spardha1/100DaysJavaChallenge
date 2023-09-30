6c1f93fcb8da35b46ad4f41f7048b4146e0fff15
Spardha1 <80118289+Spardha1@users.noreply.github.com>
2023-09-30 20:16:07 +0530

    Add files via upload

---
+++ q38.java
@@ -0,0 +1,10 @@
+public class q38 {
+        public static void main(String[] args) {
+    
+            for (int i = 1; i <= 10; i++) {
+                System.out.println(5 * i);
+            }
+        }
+    }
+    
+
