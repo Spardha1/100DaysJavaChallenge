90b5294bf431d03ee740c0620bf451afff1284f3
Spardha1 <80118289+Spardha1@users.noreply.github.com>
2023-09-30 20:16:27 +0530

    Add files via upload

---
+++ q39.java
@@ -0,0 +1,14 @@
+public class q39 {
+        public static void main(String[] args) {
+    
+            int sum = 0;
+            for (int i = 1; i <= 5; i++) {
+                sum += i;
+            }
+    
+            System.out.println("Sum of the first 5 natural numbers is: " + sum);
+        }
+    }
+    
+    
+
