d081d38928ded1f731adfffef3d4dcd09ff9b4e0
Spardha1 <80118289+Spardha1@users.noreply.github.com>
2023-09-30 20:16:42 +0530

    Add files via upload

---
+++ q40.java
@@ -0,0 +1,11 @@
+public class q40 {
+        public static void main(String[] args) {
+            int sum = 0;
+            for (int i = 2; i <= 10; i += 2) {
+                sum += i;
+            }
+    
+            System.out.println("The sum of the first 5 even numbers is: " + sum);
+        }
+    }
+    
