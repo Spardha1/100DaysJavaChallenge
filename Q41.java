4fd57bc20d01b049544a6e81ce553983f12364ec
Spardha1 <80118289+Spardha1@users.noreply.github.com>
2023-10-01 23:54:14 +0530

    Add files via upload

---
+++ Q41.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Q41 {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Enter a number: ");
+    int n = sc.nextInt();
+
+    for (int i = 1; i <= n; i++) {
+      System.out.println(i);
+    }
+    sc.close();
+  }
+}
+
+    
+
