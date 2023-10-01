5f5af290d5adfbb5aeed47d6ca6f65b8ab6e7588
Spardha1 <80118289+Spardha1@users.noreply.github.com>
2023-10-01 23:54:43 +0530

    Add files via upload

---
+++ Q43.java
@@ -0,0 +1,11 @@
+public class Q43{
+    public static void main(String[] args) {
+      int sum = 0;
+      for (int i = 2; i <= 26; i += 2) {
+        sum += i;
+      }
+  
+      System.out.println("The sum of the first 13 even numbers is " + sum);
+    }
+  }
+   
