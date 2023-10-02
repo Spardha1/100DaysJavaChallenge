9524d464ef6067420c72c64b5fd2d64a1f0d4b3e
Spardha1 <80118289+Spardha1@users.noreply.github.com>
2023-10-02 23:48:47 +0530

    Add files via upload

---
+++ Q48.java
@@ -0,0 +1,12 @@
+
+public class Q48 {
+        public static void main(String[] args) {
+            int fact = 1;
+            for (int i = 1; i <= 5; i++) {
+                fact *= i;
+            }
+            System.out.println("The factorial of 5 is: " + fact);
+        }
+    }
+    
+
