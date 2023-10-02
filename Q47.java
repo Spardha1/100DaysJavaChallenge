732b4973beb856018cd8740268fd2f4f8a869937
Spardha1 <80118289+Spardha1@users.noreply.github.com>
2023-10-02 23:48:24 +0530

    Add files via upload

---
+++ Q47.java
@@ -0,0 +1,10 @@
+
+public class Q47 {
+        public static void main(String[] args) {
+            for (char a = 'a'; a <= 'z'; a++) {
+                System.out.println(a);
+            }
+        }
+    }
+    
+
