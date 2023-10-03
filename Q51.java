d79f8e55a317d5ce8eb5aa314e566b6e59729bf4
Spardha1 <80118289+Spardha1@users.noreply.github.com>
2023-10-03 22:52:04 +0530

    Add files via upload

---
+++ Q51.java
@@ -0,0 +1,11 @@
+public class Q51 {
+
+    public static void main(String[] args) {
+        int mul = 1;
+        for (int i = 1; i <= 10; i++) {
+            mul *= i;
+        }
+        System.out.println("The product of numbers from 1 to 10 is: " + mul);
+    }
+    
+}
