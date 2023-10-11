2d40ccaa7f453cf41104ec062bd6df1d5ae33e32
Spardha1 <80118289+Spardha1@users.noreply.github.com>
2023-10-11 23:30:31 +0530

    Add files via upload

---
+++ Q89.java
@@ -0,0 +1,11 @@
+public class Q89 {
+    public static void main(String[] args) {
+        for (int i = 9; i > 0; i -= 2) {
+          for (int j = 0; j < i; j++) {
+            System.out.print("*");
+          }
+          System.out.println();
+        }
+      }
+    }
+
